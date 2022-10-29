7c479f84ce70cbfa8433d893d03ba76b8dbdbdbb
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-10-29 17:32:54 +0200

    charakterAndEminiesImp

--- public/js/game.ts
+++ public/js/game.ts
@@ -1,10 +1,7 @@
 let canvas;
-
 let world;
 
 function init() {
    canvas = document.getElementById("canvas") as HTMLCanvasElement;
    world = new World(canvas);
-
-   console.log("My character is", world.character);
 }
--- public/models/world.class.js
+++ public/models/world.class.js
@@ -2,11 +2,26 @@
 class World {
     character = new Character();
     enemies = [new Chicken(), new Chicken(), new Chicken()];
+    canvas;
     ctx;
     constructor(canvas) {
-        (this.ctx = canvas.getContext("2d")), this.draw();
+        (this.ctx = canvas.getContext("2d")), (this.canvas = canvas);
+        this.draw();
     }
     draw() {
-        this.ctx?.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        if (this.ctx != null) {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+            this.enemies.forEach((enemy) => {
+                if (this.ctx != null) {
+                    this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+                }
+            });
+            //draw wird immer wieder
+            let self = this;
+            requestAnimationFrame(function () {
+                self.draw();
+            });
+        }
     }
 }

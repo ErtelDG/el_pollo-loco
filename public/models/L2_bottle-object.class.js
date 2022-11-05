a2e964affbc16da6580043f386f785f3f6e51ba7
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-11-05 13:44:59 +0100

    save

--- public/models/L2_bottle-object.class.js
+++ public/models/L2_bottle-object.class.js
@@ -1,14 +1,16 @@
 "use strict";
 class Bottle extends MovableObject {
     IMAGES = ["img/6_salsa_bottle/1_salsa_bottle_on_ground.png", "img/6_salsa_bottle/2_salsa_bottle_on_ground.png"];
+    randomNumber = 0;
     constructor() {
         super();
         this.x = 250 + Math.random() * 5000;
         this.y = 320;
         this.height = 60;
         this.width = 50;
-        this.loadImage(this.IMAGES[0]);
+        this.randomNumber = (Math.random() * 1).toFixed(0);
         this.loadImagesBottles(this.IMAGES);
+        this.loadImage(this.IMAGES[this.randomNumber]);
     }
     loadImagesBottles(arr) {
         arr.forEach((path) => {

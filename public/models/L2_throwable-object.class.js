109dc252b3da35f9b478e51f22bd1698b1699cbf
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-11-05 14:01:30 +0100

    save

--- public/models/L2_throwable-object.class.js
+++ public/models/L2_throwable-object.class.js
@@ -1,13 +1,22 @@
 "use strict";
 class ThrowableObject extends MovableObject {
+    IMAGES_ARRAY = [
+        "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+    ];
+    imagesRotationBottle = [];
     constructor(characterX, characterY) {
         super();
         this.x = characterX;
         this.y = characterY;
         this.height = 60;
         this.width = 50;
-        this.loadImage("img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
+        this.loadImage("img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png");
+        this.loadImages(this.IMAGES_ARRAY);
         this.throw(this.x, this.y);
+        this.animate();
     }
     throw(x, y) {
         this.x = x;
@@ -17,5 +26,17 @@ class ThrowableObject extends MovableObject {
         setInterval(() => {
             this.x += 10;
         }, 25);
+    }
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imagesRotationBottle.push(img);
+        });
+    }
+    animate() {
+        setInterval(() => {
+            this.animationObjects(this.IMAGES_ARRAY, this.imagesRotationBottle);
+        }, 120);
     }
 }
--- public/models/L2_throwable-object.class.ts
+++ public/models/L2_throwable-object.class.ts
@@ -1,12 +1,24 @@
 class ThrowableObject extends MovableObject {
+   IMAGES_ARRAY = [
+      "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+      "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+      "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+      "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+   ];
+
+   imagesRotationBottle: any = [];
+   
+
    constructor(characterX: number, characterY: number) {
       super();
       this.x = characterX;
       this.y = characterY;
       this.height = 60;
       this.width = 50;
-      this.loadImage("img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
+      this.loadImage("img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png");
+      this.loadImages(this.IMAGES_ARRAY);
       this.throw(this.x, this.y);
+      this.animate();
    }
 
    throw(x: number, y: number) {
@@ -17,5 +29,19 @@ class ThrowableObject extends MovableObject {
       setInterval(() => {
          this.x += 10;
       }, 25);
+   }
+
+   loadImages(arr: any[]) {
+      arr.forEach((path: string) => {
+         let img = new Image();
+         img.src = path;
+         this.imagesRotationBottle.push(img);
+      });
+   }
+
+   animate() {
+      setInterval(() => {
+         this.animationObjects(this.IMAGES_ARRAY, this.imagesRotationBottle);
+      }, 120);
    }
 }
